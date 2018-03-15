d036cb8f2418174c1291a95d70f1053a25dbe633
1000ch <shogo.sensui@gmail.com>
2018-03-15 17:19:31 +0900

    Initial commit

---
+++ extension.js
@@ -0,0 +1,49 @@
+const vscode = require('vscode');
+const setText = require('vscode-set-text');
+const SVGO = require('svgo');
+
+exports.activate = ({ subscriptions }) => {
+	const workspaceConfig = workspace.getConfiguration('svgo');
+
+  const minify = vscode.commands.registerCommand('extension.minify', () => {
+    const { document } = vscode.window.activeTextEditor;
+
+    if (document.languageId !== 'xml' || !document.fileName.includes('.svg')) {
+      return;
+    }
+
+    const svgo = new SVGO({
+      js2svg: {
+        pretty: false
+      }
+    });
+
+    (async () => {
+      const result = await svgo.optimize(document.getText());
+      await setText(Buffer.from(result.data));
+    })();
+  });
+
+  const prettify = vscode.commands.registerCommand('extension.prettify', () => {
+    const { document } = vscode.window.activeTextEditor;
+
+    if (document.languageId !== 'xml' || !document.fileName.includes('.svg')) {
+      return;
+    }
+
+    const svgo = new SVGO({
+      js2svg: {
+        pretty: true,
+        indent: workspaceConfig.get('indent')
+      }
+    });
+
+    (async () => {
+      const result = await svgo.optimize(document.getText());
+      await setText(Buffer.from(result.data));
+    })();
+  });
+
+  subscriptions.push(minify);
+  subscriptions.push(prettify);
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+# vscode-svgo
+
+Minify SVG with [SVGO](http://github.com/svg/svgo).
+
+## Config
+
+### Indent
+
+Indent size to apply on prettify.
+
+## License
+
+[MIT](https://1000ch.mit-license.org) © [Shogo Sensui](https://github.com/1000ch)
