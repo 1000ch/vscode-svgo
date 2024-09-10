a61146b7adfced6b60e7241ba811d42de9345128
1000ch <shogosensui@gmail.com>
2024-09-10 13:48:43 +0900

    Use svgo 4.0.0-rc.1

--- src/extension.ts
+++ src/extension.ts
@@ -14,9 +14,7 @@ import {
   loadConfig,
   optimize,
   builtinPlugins,
-  type Config,
-  type Output,
-  type PluginConfig,
+  type Config
 } from 'svgo';
 
 const presetDefault = builtinPlugins.find(p => p.name === 'preset-default');
@@ -36,13 +34,13 @@ function getPluginConfig(): Config {
 
   // Use 'preset-default' plugin to override defaults
   // https://github.com/svg/svgo#configuration
-  const defaultPlugin: PluginConfig = {
+  const defaultPlugin = {
     name: 'preset-default',
     params: {
       overrides: {},
     },
   };
-  const otherPlugins: PluginConfig[] = [];
+  const otherPlugins = [];
 
   for (const plugin of builtins) {
     // If plugin is configured by workspace config
@@ -53,11 +51,11 @@ function getPluginConfig(): Config {
     if (defaultPlugins.has(plugin)) {
       defaultPlugin.params.overrides[plugin] = svgoConfig.get<boolean>(plugin);
     } else if (svgoConfig.get<boolean>(plugin)) {
-      otherPlugins.push(plugin as PluginConfig);
+      otherPlugins.push(plugin);
     }
   }
 
-  const plugins: PluginConfig[] = [defaultPlugin, ...otherPlugins];
+  const plugins = [defaultPlugin, ...otherPlugins];
   const pluginConfig: Config = {plugins};
 
   return pluginConfig;
@@ -99,7 +97,7 @@ async function processTextEditor(textEditor: TextEditor, config?: Config) {
 
   const mergedConfig = await getConfig(config);
   const text = textEditor.document.getText();
-  const {data}: Output = optimize(text, mergedConfig);
+  const {data} = optimize(text, mergedConfig);
   await setText(data, textEditor);
 }
 
